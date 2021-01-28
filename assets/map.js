9770147b8975238a1bee9cd643eef295e5348f0d
Ben Gross <benrgross@gmail.com>
2021-01-28 15:37:56 -0500

    google places w/ multiple results

--- assets/map.js
+++ assets/map.js
@@ -37,7 +37,8 @@ function initMap() {
   var request = {
     location: sydney,
     radius: "500",
-    type: ["spaghetti"],
+    type: ["restaurant"],
+    query: "sushi",
   };
 
   service = new google.maps.places.PlacesService(map);
@@ -48,6 +49,7 @@ function callback(results, status) {
   if (status == google.maps.places.PlacesServiceStatus.OK) {
     for (var i = 0; i < results.length; i++) {
       createMarker(results[i]);
+      console.log("results", results);
     }
   }
 }
