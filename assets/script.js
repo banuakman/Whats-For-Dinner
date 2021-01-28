a731b8f458ddcdebdb80161d9c7ea106f4bc1ed3
Ben Gross <benrgross@gmail.com>
2021-01-28 13:40:24 -0500

    added map with map js script. and css #map. also added mobil view meta tag.

--- assets/script.js
+++ assets/script.js
@@ -14,53 +14,54 @@
 
 // // ---------------Google Places AJAX Call ---------------
 
-var map;
-var service;
-var infowindow;
+// var map;
+// var service;
+// var infowindow;
 
-function initMap() {
-  var sydney = new google.maps.LatLng(-33.867, 151.195);
+// function initMap() {
+//   var sydney = new google.maps.LatLng(-33.867, 151.195);
 
-  infowindow = new google.maps.InfoWindow();
+//   infowindow = new google.maps.InfoWindow();
 
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: sydney,
-    zoom: 15,
-  });
+//   map = new google.maps.Map(document.getElementById(".map"), {
+//     center: sydney,
+//     zoom: 15,
+//   });
 
-  var request = {
-    query: "Museum of Contemporary Art Australia",
-    fields: ["name", "geometry"],
-  };
+//   var request = {
+//     query: "Museum of Contemporary Art Australia",
+//     fields: ["name", "geometry"],
+//   };
 
-  var service = new google.maps.places.PlacesService(map);
+//   var service = new google.maps.places.PlacesService(map);
 
-  service.findPlaceFromQuery(request, function (results, status) {
-    if (status === google.maps.places.PlacesServiceStatus.OK) {
-      for (var i = 0; i < results.length; i++) {
-        createMarker(results[i]);
-      }
-      map.setCenter(results[0].geometry.location);
-    }
-  });
-}
+// service.findPlaceFromQuery(request, function (results, status) {
+//   if (status === google.maps.places.PlacesServiceStatus.OK) {
+//     for (var i = 0; i < results.length; i++) {
+//       createMarker(results[i]);
+//     }
+//     map.setCenter(results[0].geometry.location);
+//   }
+// });
 
-$.ajax(settings).done(function (response) {
-  console.log(response.recipes);
-  displayReceipe(response.recipes);
-});
+// initMap();
 
-function displayReceipe(recipes) {
-  for (var i = 0; i < recipes.length; i++) {
-    var title = recipes[i].title;
-    var instructions = recipes[i].instructions;
-    var liEl = $("<li>").html(
-      "<h4>" + title + "</h4><p>" + instructions + "</p>"
-    );
-    var aEl = $("<a>")
-      .attr("href", recipes[i].sourceUrl)
-      .text(recipes[i].sourceUrl);
-    liEl.append(aEl);
-    $(".recipes").append(liEl);
-  }
-}
+// $.ajax(settings).done(function (response) {
+//   console.log(response.recipes);
+//   displayReceipe(response.recipes);
+// });
+
+// function displayReceipe(recipes) {
+//   for (var i = 0; i < recipes.length; i++) {
+//     var title = recipes[i].title;
+//     var instructions = recipes[i].instructions;
+//     var liEl = $("<li>").html(
+//       "<h4>" + title + "</h4><p>" + instructions + "</p>"
+//     );
+//     var aEl = $("<a>")
+//       .attr("href", recipes[i].sourceUrl)
+//       .text(recipes[i].sourceUrl);
+//     liEl.append(aEl);
+//     $(".recipes").append(liEl);
+//   }
+// }
