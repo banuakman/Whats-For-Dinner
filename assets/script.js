c3e5e59d422ea5c6e170c5499312299ee897801a
Angelina Ma <angelina890308@gmail.com>
2021-01-28 11:19:20 -0800

    add google place

--- assets/script.js
+++ assets/script.js
@@ -59,29 +59,36 @@ var service;
 var infowindow;
 
 function initMap() {
-  var sydney = new google.maps.LatLng(-33.867, 151.195);
-
+  const currentLoc = new google.maps.LatLng(-33.867, 151.195);
   infowindow = new google.maps.InfoWindow();
-
   map = new google.maps.Map(document.getElementById("map"), {
-    center: sydney,
+    center: currentLoc,
     zoom: 15,
   });
-
-  var request = {
-    query: "Museum of Contemporary Art Australia",
+  const request = {
+    query: "retaurants",
     fields: ["name", "geometry"],
   };
-
-  var service = new google.maps.places.PlacesService(map);
-
-  service.findPlaceFromQuery(request, function (results, status) {
+  service = new google.maps.places.PlacesService(map);
+  service.findPlaceFromQuery(request, (results, status) => {
     if (status === google.maps.places.PlacesServiceStatus.OK) {
-      for (var i = 0; i < results.length; i++) {
+      for (let i = 0; i < results.length; i++) {
         createMarker(results[i]);
+        console.log("search result" + i + ":" + results);
       }
       map.setCenter(results[0].geometry.location);
     }
+  });
+}
+
+function createMarker(place) {
+  const marker = new google.maps.Marker({
+    map,
+    position: place.geometry.location,
+  });
+  google.maps.event.addListener(marker, "click", () => {
+    infowindow.setContent(place.name);
+    infowindow.open(map);
   });
 }
 
