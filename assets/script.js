0bfc4329b75b47cb6314f446ddc865c6cf4e52a2
Ben Gross <benrgross@gmail.com>
2021-01-28 11:50:35 -0500

    beggining of psudo code.

--- assets/script.js
+++ assets/script.js
@@ -1,48 +1,66 @@
+//---------- What are the Dependencies -----------
+// - DOM Elements
+// - variables
+
+// ----------- Data ----------------------
+//--
+
+// ----------- Functions ------------------
+
+// ---------- User Interaction -----------------
+
+// when the user pressed the eat out button --->
+// a dish gets randomly generated
+
 // // ---------------Google Places AJAX Call ---------------
 
-// query URL with two parameters Location and api key
-var loc = "Brooklyn+NewYork";
-const APIkey = "AIzaSyB4CRiYoGBXcZ0BS3z8UK1lFFILHs3VOKQ";
-var queryURL =
-  "https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants+toronto+canada&key=" +
-  APIkey;
+var map;
+var service;
+var infowindow;
 
-// ---------- ajaz call ---------
-$.ajax({
-  url: queryURL,
-  method: "GET",
-}).then(function (response) {
-  console.log(response);
-});
+function initMap() {
+  var sydney = new google.maps.LatLng(-33.867, 151.195);
 
-/**recipes api call to spoonacular*/
-const settings = {
-	"async": true,
-    "crossDomain": true,
-    //current set to 10 randome recipes, can be changed
-	"url": "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/random?number=10",
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-key": "130332a6ccmshd9ecdd5f1b0a4d7p12e090jsnf616f928de59",
-		"x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
-	}
-};
+  infowindow = new google.maps.InfoWindow();
+
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: sydney,
+    zoom: 15,
+  });
+
+  var request = {
+    query: "Museum of Contemporary Art Australia",
+    fields: ["name", "geometry"],
+  };
+
+  var service = new google.maps.places.PlacesService(map);
+
+  service.findPlaceFromQuery(request, function (results, status) {
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
+      for (var i = 0; i < results.length; i++) {
+        createMarker(results[i]);
+      }
+      map.setCenter(results[0].geometry.location);
+    }
+  });
+}
 
 $.ajax(settings).done(function (response) {
-    console.log(response.recipes);
-    displayReceipe(response.recipes);
+  console.log(response.recipes);
+  displayReceipe(response.recipes);
 });
 
 function displayReceipe(recipes) {
-    for (var i = 0; i < recipes.length; i++) {
-        var title = recipes[i].title;
-        var instructions = recipes[i].instructions;
-        var liEl = $("<li>").html("<h4>" +title +"</h4><p>" +instructions +"</p>");
-        var aEl = $("<a>").attr("href", recipes[i].sourceUrl).text(recipes[i].sourceUrl);
-        liEl.append(aEl);
-        $(".recipes").append(liEl);
-    }
-
-    
-
+  for (var i = 0; i < recipes.length; i++) {
+    var title = recipes[i].title;
+    var instructions = recipes[i].instructions;
+    var liEl = $("<li>").html(
+      "<h4>" + title + "</h4><p>" + instructions + "</p>"
+    );
+    var aEl = $("<a>")
+      .attr("href", recipes[i].sourceUrl)
+      .text(recipes[i].sourceUrl);
+    liEl.append(aEl);
+    $(".recipes").append(liEl);
+  }
 }
