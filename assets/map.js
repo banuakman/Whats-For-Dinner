44f5085095637f2b51126817360a4c7949bfdfdd
Ben Gross <benrgross@gmail.com>
2021-01-29 13:32:28 -0500

    get places id and info window pops up

--- assets/map.js
+++ assets/map.js
@@ -1,7 +1,6 @@
 // variables set from google maps docs
 var map;
 var service;
-var infowindow;
 let infoWindow;
 
 // ---- build and place show map button ----
@@ -18,13 +17,17 @@ showMap.click(function () {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(
       (position) => {
-        //--------- recieves lat and lon and puts them in pos variable --------
+        if (position) {
+          // document.getElementById("map").classList.remove("hide");
+        }
+        // document.getElementById("map").classList.remove("hide");
+        //--------- receives lat and lon and puts them in pos variable --------
         var pos = {
           lat: position.coords.latitude,
           lng: position.coords.longitude,
         };
         // ----------  gennerates new map with coordinates from pos -----------
-        infoWindow = new google.maps.InfoWindow();
+        // infoWindow = new google.maps.InfoWindow();
         map = new google.maps.Map(document.getElementById("map"), {
           center: pos,
           zoom: 15,
@@ -71,39 +74,54 @@ function nearBySearchHandler(results, status) {
   // --------- loop results and add marker ---------
   if (status == google.maps.places.PlacesServiceStatus.OK) {
     for (var i = 0; i < results.length; i++) {
-      createMarker(results[i]);
+      console.log("places 1", results[i]);
+      getPlaceID(map, results[i]);
+      // createMarker(results[i]);
+      // google.maps.event.addListener(results[i], "click", function () {
+      //   console.log("hello marker");
+      //   infoWindow.setContent(
+      // "<div><strong>" +
+      //   place.name +
+      //   "</strong><br>" +
+      //   "Place ID: " +
+      //   place.place_id +
+      //   "<br>" +
+      //   place.formatted_address +
+      //   "</div>"
+      //   );
+      //   infoWindow.open(map, marker);
+      // });
     }
   }
 }
 
 function textSearchHandler(results, status) {
-    // --------- loop results and add marker ---------
-    if (status == google.maps.places.PlacesServiceStatus.OK) {
-      var lat = results[0].geometry.location.lat();
-      var lng = results[0].geometry.location.lng();
-      
-      map.setCenter({lat: lat, lng: lng}); 
+  // --------- loop results and add marker ---------
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+    var lat = results[0].geometry.location.lat();
+    var lng = results[0].geometry.location.lng();
 
-      for (var i = 0; i < results.length; i++) {
-        // console.log(results[i]);
-        createMarker(results[i]);
-      }
+    map.setCenter({ lat: lat, lng: lng });
+
+    for (var i = 0; i < results.length; i++) {
+      createMarker(results[i]);
     }
   }
+}
 
 function textSearch() {
-    /*without geolocation center: either we create map with a default center location, e.g. New York or without center, if we want to support search to a different city or area far away, do not set center 
+  /*without geolocation center: either we create map with a default center location, e.g. New York or without center, if we want to support search to a different city or area far away, do not set center 
     Center will set once we have result from the search and set the first result location as the map center
     */
-    map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 15,
-    });
-    service = new google.maps.places.PlacesService(map);
+  map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 15,
+  });
+  service = new google.maps.places.PlacesService(map);
   //---------------text search --------------------
-    var textSearchRequest = {
-        query: 'shake shake in new york',
-        type: 'restaurant'
-    }
+  var textSearchRequest = {
+    query: "shake shake in new york",
+    type: "restaurant",
+  };
 
   service.textSearch(textSearchRequest, textSearchHandler);
 }
@@ -114,11 +132,36 @@ function createMarker(place) {
     map,
     position: place.geometry.location,
   });
-  // ----------- not working/ make our own click event and dispay? -------------
+  // ----------- displays infor in window when marker clicked --------
   google.maps.event.addListener(marker, "click", () => {
-    infoWindow.setContent(place.name);
-    infoWindow.open(map);
+    console.log("webisite", place.website);
+    infoWindow.setContent(
+      "<div><strong>" +
+        place.name +
+        "</strong><br>" +
+        "<br>" +
+        place.formatted_address +
+        "<br>" +
+        `<a href="${place.website}">link to website </a>` +
+        "</div>"
+    );
+    infoWindow.open(map, marker);
   });
 }
 
-// need to add serrate function that calls the info and displays it when marker is clicked or displays it to a list we create
+// ----- Gets Place Info and Intitiates Create marker ------------
+function getPlaceID(map, results) {
+  console.log("hello");
+  const request = {
+    placeId: results.place_id,
+    fields: ["name", "formatted_address", "place_id", "geometry", "website"],
+  };
+  infoWindow = new google.maps.InfoWindow();
+  service = new google.maps.places.PlacesService(map);
+  service.getDetails(request, (place, status) => {
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
+      createMarker(place);
+      console.log("info place", place);
+    }
+  });
+}
