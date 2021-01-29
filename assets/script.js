482cf9fe8ed592f6b8aaf2bb51b1c51bc29f81b0
banuakman <banu@asterya.com>
2021-01-28 22:01:23 -0500

    Spoonacular API call changed to get only main course items

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,73 @@
-//---------- What are the Dependencies -----------
+// Dependencies ==========================================
 // - DOM Elements
 // - variables
 
-// ----------- Data ----------------------
-//
 
-// ----------- Functions ------------------
 
-// ---------- User Interaction -----------------
 
-// when the user pressed the eat out button --->
-// a dish gets randomly generated
+// Data ==================================================
 
-// When the user clicks the "Random" button (on page 1)
+
+
+
+// Functions =============================================
+
+// AJAX Call Spoonacular API 
+const spoonacularSettings = {
+  async: true,
+  crossDomain: true,
+  url:
+    "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/random?number=3&tags=dinner,maincourse,sidedish",
+  method: "GET",
+  headers: {
+    "x-rapidapi-key": "130332a6ccmshd9ecdd5f1b0a4d7p12e090jsnf616f928de59",
+    "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+  },
+};
+
+$.ajax(spoonacularSettings).done(function (response) {
+  console.log(response.recipes);
+  displayRandom(response.recipes);
+});
+
+// Display 3 Dishes with Title & Picture
+function displayRandom(recipes) {
+   for (var i = 0; i < recipes.length; i++) {
+     var title = recipes[i].title;
+     var liEl = $("<li>").html("<h4>" + title + "</h4>");
+     var imgEl = $("<img>")
+       .attr("src", recipes[i].image)
+       .text("alt", recipes[i].title);
+     liEl.append(imgEl);
+     $(".recipes").append(liEl);
+   }
+ }
+
+
+// Display Recipe Details
+function displayRecipe(recipes) {
+  for (var i = 0; i < recipes.length; i++) {
+    var title = recipes[i].title;
+    var instructions = recipes[i].instructions;
+    var liEl = $("<li>").html(
+      "<h4>" + title + "</h4><p>" + instructions + "</p>"
+    );
+    var aEl = $("<a>")
+      .attr("href", recipes[i].sourceUrl)
+      .text(recipes[i].sourceUrl);
+    liEl.append(aEl);
+    $(".recipes").append(liEl);
+  }
+}
+
+// User Interaction =====================================
+
+// When the user clicks the "What's for Dinner" button (on page 1)
 // call the function to generate three random dishes
 // and display them.
-$("#random-button").on("click", function () {});
+$("#random-button").on("click", function () {
+  
+});
 
 // When the user clicks on the "Regenerate" button
 // (on page 2) call a function to get three
@@ -38,37 +90,4 @@ $("#back-button").on("click", function () {});
 // function to display the recipe detail page.
 $("#image-click").on("click", function () {});
 
-// // ---------------Google Places AJAX Call ---------------
 
-const spoonacularSettings = {
-  async: true,
-  crossDomain: true,
-  //current set to 10 randome recipes, can be changed
-  url:
-    "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/random?number=10",
-  method: "GET",
-  headers: {
-    "x-rapidapi-key": "130332a6ccmshd9ecdd5f1b0a4d7p12e090jsnf616f928de59",
-    "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
-  },
-};
-
-$.ajax(spoonacularSettings).done(function (response) {
-  console.log(response.recipes);
-  displayReceipe(response.recipes);
-});
-
-function displayReceipe(recipes) {
-  for (var i = 0; i < recipes.length; i++) {
-    var title = recipes[i].title;
-    var instructions = recipes[i].instructions;
-    var liEl = $("<li>").html(
-      "<h4>" + title + "</h4><p>" + instructions + "</p>"
-    );
-    var aEl = $("<a>")
-      .attr("href", recipes[i].sourceUrl)
-      .text(recipes[i].sourceUrl);
-    liEl.append(aEl);
-    $(".recipes").append(liEl);
-  }
-}
