2f6ed1bf71f1d6b3bc23480646ce496dcd9c508b
Ben Gross <benrgross@gmail.com>
2021-01-31 12:59:22 -0500

    style for maplist.

--- assets/script.js
+++ assets/script.js
@@ -28,8 +28,8 @@ function generateRandomRecipes() {
       "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/random?number=3&tags=dinner,maincourse,sidedish",
     method: "GET",
     headers: {
-    //  "x-rapidapi-key": "130332a6ccmshd9ecdd5f1b0a4d7p12e090jsnf616f928de59",
-    //  "x-rapidapi-key": "aec4b3ea07msha3618e894254591p168662jsnb96bf9a67318",
+      //  "x-rapidapi-key": "130332a6ccmshd9ecdd5f1b0a4d7p12e090jsnf616f928de59",
+      //  "x-rapidapi-key": "aec4b3ea07msha3618e894254591p168662jsnb96bf9a67318",
       "x-rapidapi-key": "33cd4a2c49mshf76dee9bb71dc52p1dff08jsn917a329ffdff",
       "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
     },
@@ -47,16 +47,9 @@ function displayRandom(recipes) {
   $(".randomRecipes").html("");
 
   for (var i = 0; i < recipes.length; i++) {
-<<<<<<< HEAD
-    var cellEl = $("<div>").attr("class", "columns large-4");
+    var cellEl = $("<div>").attr("class", "columns large-4 medium-4");
     var cardEl = $("<div>").attr("class", "card randomRecipeCard");
     cellEl.append(cardEl);
-=======
-    var cellEl = $("<div>").attr("class", "columns large-4 medium-4");
-    var cardEl = $("<div>")
-      .attr("class", "card randomRecipeCard");
-    cellEl.append(cardEl)
->>>>>>> main
 
     var title = recipes[i].title;
     var titleEl = $("<div>")
@@ -186,14 +179,9 @@ function displayRecipeDetail(singleRecipe) {
   var aEl = $("<a>")
     .attr("href", singleRecipe.sourceUrl)
     .html(singleRecipe.sourceUrl);
-<<<<<<< HEAD
-  liEl.append(aEl);
-  $(".recipeDetailsSummary").append(liEl);
-=======
   detailsEl.append(aEl);
   $(".recipeDetailsSummary").append(detailsEl);
   //----------------------your code ends here-------------------------------
->>>>>>> main
 }
 
 //----------------------your code ends here-------------------------------
