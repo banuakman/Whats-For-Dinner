1defa0f1da45d1a1250b8ab55c5640f84c5d139a
Ben Gross <benrgross@gmail.com>
2021-01-31 19:33:09 -0500

    merge commit.

--- assets/script.js
+++ assets/script.js
@@ -39,8 +39,6 @@ function generateRandomRecipes() {
     recipesObject = response.recipes;
     console.log(recipesObject);
     displayRandom(response.recipes);
-
-
   });
 }
 
@@ -110,7 +108,7 @@ function displayRandom(recipes) {
     var dropDown = $(this).next();
     if (dropDown.is(":hidden")) {
       dropDown.show();
-    } 
+    }
 
     //this is the <a> tag and dropdown is next sibling node/element
     // var selectedIndex = $(this).attr("data-index");
@@ -157,12 +155,12 @@ function hideUnhideDiv(divToUnhide) {
       recipeDetails.classList.add("hide");
       mapContainer.classList.add("hide");
       break;
-    case "recipe-details": 
+    case "recipe-details":
       recipeDetails.classList.remove("hide");
       dishDisplay.classList.add("hide");
       mapContainer.classList.add("hide");
       break;
-    case "map-container": 
+    case "map-container":
       mapContainer.classList.remove("hide");
       dishDisplay.classList.add("hide");
       recipeDetails.classList.add("hide");
@@ -171,8 +169,8 @@ function hideUnhideDiv(divToUnhide) {
         "Searching Results For:" + " " + localStorage.getItem("searchTitle")
       );
       break;
-  };
- // recipeDetails.classList.remove("hide");
+  }
+  // recipeDetails.classList.remove("hide");
 }
 
 // --------Display Restaurant Details  (Johanna)
